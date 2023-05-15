1cb44c0df3866f9f5bf3a6dfec0b8b77f580cf5a
Lyrismet <ana0203tir@gmail.com>
2023-05-15 20:32:08 +0700

    feat: add func scrollTo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'burgers-angular';
+  scrollTo(target: HTMLElement) {
+    target.scrollIntoView({ behavior: 'smooth' });
+  }
 }
